b6e213e878a7cdc66fc61ce71a51fb60f73a4342
Tobias Wylega <tobias@wylega.de>
2024-03-26 16:55:09 +0100

    add prime-ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalendarModule} from "primeng/calendar";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'study-modules';
+  date: any;
 }
