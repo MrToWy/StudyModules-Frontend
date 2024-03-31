05e322f57ed7f7ab6887954ef42435a1f06ecd3a
Tobias Wylega <github@wylega.de>
2024-03-31 20:13:03 +0200

    login redirects to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import {CalendarModule} from "primeng/calendar";
 import {FormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../shared/auth/auth.service";
+import {AuthModule} from "../shared/auth/auth.module";
+import {MainComponent} from "./main/main.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarModule, FormsModule, LoginComponent],
+  imports: [RouterOutlet, CalendarModule, FormsModule, LoginComponent, NgIf, AuthModule, MainComponent],
+  providers: [AuthService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'study-modules';
   date: any;
+
+  constructor(protected authService: AuthService) {
+  }
 }
--- src/shared/auth/auth.service.ts
+++ src/shared/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 
 @Injectable()
-export class LoginService {
+export class AuthService {
   private loginURL: string = environment.backendURL + "auth/login";
 
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(this.loginURL, {
+  login(username: string, password: string) {
+    let task = this.http.post<LoginResponse>(this.loginURL, {
       username: username,
       password: password
     });
+
+    task.subscribe(response => {
+      localStorage.setItem('token', response.token)
+    })
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('token') !== null;
   }
 }
 
