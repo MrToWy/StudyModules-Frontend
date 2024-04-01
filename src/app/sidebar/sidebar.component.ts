7f556c412ddaf475b627612b41a3e09c442b3337
Tobias Wylega <github@wylega.de>
2024-04-01 19:59:07 +0200

    hide logout button in sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import {ButtonModule} from "primeng/button";
 import {AvatarModule} from "primeng/avatar";
 import {StyleClassModule} from "primeng/styleclass";
 import {RippleModule} from "primeng/ripple";
+import {AuthService} from "../../shared/auth/auth.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -19,5 +20,13 @@ import {RippleModule} from "primeng/ripple";
   styleUrl: './sidebar.component.sass'
 })
 export class SidebarComponent {
+  private authService: AuthService;
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
   sidebarVisible: boolean = true;
+
+  logout() {
+    this.authService.logout();
+  }
 }
