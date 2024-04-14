a1160b027e22eda38f269d384feb2323e255aa5f
Tobias Wylega <github@wylega.de>
2024-04-14 18:03:38 +0200

    Add Module Edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {FacultyDetailComponent} from "./faculty-detail/faculty-detail.component"
 import {FacultiesComponent} from "./faculties/faculties.component";
 import {CourseDetailComponent} from "./course-detail/course-detail.component";
 import {ModuleDetailComponent} from "./module-detail/module-detail.component";
+import {ModuleEditComponent} from "./module-edit/module-edit.component";
 
 export const routes: Routes = [
   { path: 'faculties', component: FacultiesComponent },
   { path: 'faculty/:id', component: FacultyDetailComponent },
   { path: 'faculty/:id/course/:id', component: CourseDetailComponent },
   { path: 'faculty/:id/course/:id/module/:id', component: ModuleDetailComponent},
+  { path: 'faculty/:id/course/:id/module/:id/edit', component: ModuleEditComponent},
   { path: '', redirectTo: '/faculties', pathMatch: 'full' }
 ];
--- src/app/module-detail/module-detail.component.ts
+++ src/app/module-detail/module-detail.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-module-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './module-detail.component.html',
   styleUrl: './module-detail.component.sass'
 })
 export class ModuleDetailComponent {
 
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+
+  async switchToEditMode() {
+    await this.router.navigate(["edit"], {relativeTo: this.route});
+  }
 }
