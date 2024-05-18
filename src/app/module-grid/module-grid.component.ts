a0c41b7dc2a2498b4dfb0fb6e9779462ddb69abe
Tobias Wylega <github@wylega.de>
2024-05-18 23:36:31 +0200

    style table

--- src/app/module-grid/module-grid.component.ts
+++ src/app/module-grid/module-grid.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {TableModule} from "primeng/table";
 import {ModuleDto, ModuleService} from "../../shared/module/module.service";
+import {TagModule} from "primeng/tag";
+import {TooltipModule} from "primeng/tooltip";
 
 @Component({
   selector: 'app-module-grid',
   standalone: true,
   imports: [
-    TableModule
+    TableModule,
+    TagModule,
+    TooltipModule
   ],
   providers: [ModuleService],
   templateUrl: './module-grid.component.html',
--- src/shared/module/module.service.ts
+++ src/shared/module/module.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from "../../environments/environment";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 
 @Injectable()
 export class ModuleService {
@@ -9,9 +9,22 @@ export class ModuleService {
 
   constructor(private http: HttpClient) { }
 
-  getAll() {
-    return this.http.get<ModuleDto[]>(this.moduleURL);
-  }
+  getAll(): Observable<ModuleDto[]> {
+  return this.http.get<any[]>(this.moduleURL).pipe(
+    map((modules) =>
+      modules.map((module) => ({
+        id: module.id,
+        name: module.translations[0].name,  // Assumed 'name' is in 'translations[0].name'
+        abbreviation: module.abbreviation,
+        course: module.degreeProgram.abbreviation,
+        course_name: module.degreeProgram.translations[0].name,  // Assumed 'course_name' is in 'translations[0].name'
+        semester: module.semester,
+        responsible: `${module.responsible.firstName} ${module.responsible.lastName}`,
+        facultyId: module.degreeProgramId   // Assumed 'facultyId' is 'degreeProgramId'
+      }))
+    )
+  );
+}
 
   get(moduleId: number) {
     return this.http.get<ModuleDto>(this.moduleURL + "/" + moduleId);
@@ -24,7 +37,10 @@ export class ModuleService {
 
 export interface ModuleDto {
   id: number;
+  name: string;
   abbreviation: string;
-  courseId: number;
+  course: string;
+  semester: string;
+  responsible: string;
   facultyId: number;
 }
