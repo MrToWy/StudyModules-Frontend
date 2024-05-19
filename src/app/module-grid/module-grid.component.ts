7afdabe3850e5bb998dba5f9f6d5575cee8b7a82
Tobias Wylega <github@wylega.de>
2024-05-19 17:48:20 +0200

    add filter to table

--- src/app/module-grid/module-grid.component.ts
+++ src/app/module-grid/module-grid.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TableModule} from "primeng/table";
 import {ModuleDto, ModuleService} from "../../shared/module/module.service";
 import {TagModule} from "primeng/tag";
 import {TooltipModule} from "primeng/tooltip";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {MultiSelectModule} from "primeng/multiselect";
+import {AvatarModule} from "primeng/avatar";
 
 @Component({
   selector: 'app-module-grid',
@@ -10,20 +14,36 @@ import {TooltipModule} from "primeng/tooltip";
   imports: [
     TableModule,
     TagModule,
-    TooltipModule
+    TooltipModule,
+    DropdownModule,
+    FormsModule,
+    MultiSelectModule,
+    AvatarModule
   ],
   providers: [ModuleService],
   templateUrl: './module-grid.component.html',
   styleUrl: './module-grid.component.sass'
 })
 export class ModuleGridComponent {
+  users!: any[];
+  statuses!: any[];
+  selectedUser: any;
+  selectedvalue: any;
 
   constructor(moduleService: ModuleService) {
-    moduleService.getAll().subscribe(modules => this.modules = modules);
+    moduleService.getAll().subscribe(
+      modules => {
+            this.modules = modules;
+            this.statuses = [...new Set(modules.map(module => module.course))];
+            this.users = [...new Set(modules.map(module => module.responsible))];
+            console.log(this.users)
+        });
   }
 
   module: ModuleDto | undefined;
   modules: ModuleDto[] = [];
 
   protected readonly console = console;
+
+
 }
