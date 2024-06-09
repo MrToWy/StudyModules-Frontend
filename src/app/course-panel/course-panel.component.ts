87321bb7906868e29908840f16eb718a30977398
Tobias Wylega <github@wylega.de>
2024-06-09 16:37:49 +0200

    add course panel

---
+++ src/app/course-panel/course-panel.component.ts
@@ -0,0 +1,194 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {MenuModule} from "primeng/menu";
+import {PanelModule} from "primeng/panel";
+import {MenuItem, MessageService, SharedModule} from "primeng/api";
+import {CloneCourseDto, CourseDto, CourseService} from "../../shared/course/course.service";
+import {CheckboxModule} from "primeng/checkbox";
+import {DialogModule} from "primeng/dialog";
+import {InputTextModule} from "primeng/inputtext";
+import {JobsComponent} from "../jobs/jobs.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {PaginatorModule} from "primeng/paginator";
+import {StepperModule} from "primeng/stepper";
+import {CreateJobResponse, JobService} from "../../shared/job/job.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../shared/auth/auth.service";
+import {LanguageDto, LanguageService} from "../../shared/language/language.service";
+
+@Component({
+  selector: 'app-course-panel',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    MenuModule,
+    PanelModule,
+    SharedModule,
+    CheckboxModule,
+    DialogModule,
+    InputTextModule,
+    JobsComponent,
+    NgForOf,
+    NgIf,
+    PaginatorModule,
+    StepperModule
+  ],
+  templateUrl: './course-panel.component.html',
+  styleUrl: './course-panel.component.sass'
+})
+export class CoursePanelComponent implements OnInit{
+  deleted: boolean = false;
+
+  items: MenuItem[] | undefined;
+  waitForJobGuids: string[] = [];
+  languages: LanguageDto[] = [];
+
+  deleteDialogVisible = false;
+  cloneDialogVisible = false;
+  cloning = false;
+  refreshPdfDialogVisible = false;
+  generatingTexFiles = false;
+  jobsAreDone = false;
+  selectedLanguageIds: number[] = [];
+
+  selectedCourse: CourseDto | undefined;
+  cloneCourseDto: CloneCourseDto = {
+    name: undefined,
+    abbreviation: undefined,
+    pruefungsordnung: undefined
+  }
+
+  @Output() coursesChanged = new EventEmitter<void>();
+
+  @Input() course!: CourseDto;
+
+  constructor(
+              private languageService: LanguageService,
+              private router: Router,
+    private courseService: CourseService,
+              private jobService: JobService,
+              private route: ActivatedRoute,
+              protected authService: AuthService,
+              private messageService: MessageService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'PDF generieren',
+        icon: 'pi pi-fw pi-pencil',
+        command: () => {
+          this.refreshPdf(this.course);
+        }
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash',
+        command: () => {
+          this.deleteCourse(this.course)
+        }
+      },
+      {
+        label: 'Clone',
+        icon: 'pi pi-fw pi-clone',
+        command: () => {
+          this.cloneCourse(this.course);
+        }
+      }
+    ];
+
+    this.languageService.getLanguages().subscribe(languages => {
+      this.languages = languages;
+      this.selectedLanguageIds = languages.map(language => language.id);
+    });
+    }
+
+    async selectCourse(course: CourseDto) {
+    await this.router.navigate(['course', course.id], {relativeTo: this.route});
+  }
+
+  cloneCourse(course: CourseDto) {
+    this.selectedCourse = course;
+    this.cloneCourseDto = {
+      name: course.translations[0].name,
+      abbreviation: course.abbreviation,
+      pruefungsordnung: course.translations[0].pruefungsordnung
+    }
+    console.log(course);
+
+    this.cloneDialogVisible = true;
+  }
+
+  deleteCourse(course: CourseDto) {
+    this.selectedCourse = course;
+    this.deleteDialogVisible = true;
+  }
+
+  async onCourseDeleted() {
+    if (!this.selectedCourse) return;
+
+    this.courseService.delete(this.selectedCourse.id).subscribe(() => {
+      this.deleted = true;
+      this.deleteDialogVisible = false;
+    });
+  }
+
+  async onCourseCloned() {
+    if (!this.selectedCourse) return;
+    this.cloning = true;
+
+    this.courseService.cloneCourse(this.selectedCourse.id, this.cloneCourseDto).subscribe((newCourseId: number) => {
+      this.courseService.get(newCourseId).subscribe(
+        course => {
+          this.coursesChanged.emit();
+          this.cloneDialogVisible = false;
+          this.cloning = false;
+        }
+      );
+    });
+  }
+
+  downloadPdf(course: CourseDto) {
+    this.languageService.getLanguageByCode(this.languageService.languageCode).subscribe(language => {
+        window.open(this.courseService.downloadPdfUrl(course.id, language[0].id), '_blank');
+    });
+  }
+
+  refreshPdf(course: CourseDto) {
+    this.selectedCourse = course;
+    this.refreshPdfDialogVisible = true;
+  }
+
+  startPdfGeneration(nextCallback: any) {
+    nextCallback.emit();
+
+    this.generatingTexFiles = true;
+    let finished = 0;
+
+    this.selectedLanguageIds.forEach(languageId => {
+      this.jobService.createNew(languageId, this.selectedCourse?.id!).subscribe((response: CreateJobResponse) => {
+        finished++;
+        const guid = response.guid;  // Extract the guid property
+        this.waitForJobGuids = [...this.waitForJobGuids, guid];
+        if (finished === this.selectedLanguageIds.length) {
+          this.generatingTexFiles = false;
+          this.messageService.add({severity: 'success', summary: 'Success', detail: 'PDF generation started'});
+        }
+      });
+    });
+  }
+
+  confirmPdfPublish() {
+    this.refreshPdfDialogVisible = false;
+    this.messageService.add({severity: 'success', summary: 'Success', detail: 'PDF successfully published'});
+  }
+
+  cancelPdfPublish() {
+    this.refreshPdfDialogVisible = false;
+  }
+
+  onJobsDone() {
+    this.jobsAreDone = true;
+  }
+}
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ToastModule} from "primeng/toast";
 import {TableModule} from "primeng/table";
-import {MessageService} from "primeng/api";
+import {MenuItem, MessageService} from "primeng/api";
 import {ButtonModule} from "primeng/button";
 import {RippleModule} from "primeng/ripple";
-import {ActivatedRoute, Router, RouterLink} from "@angular/router";
-import {CloneCourseDto, CourseDto, CourseService} from "../../shared/course/course.service";
+import {Router, RouterLink} from "@angular/router";
+import {CourseDto, CourseService} from "../../shared/course/course.service";
 import {DepartmentService} from "../../shared/department/department.service";
 import {DialogModule} from "primeng/dialog";
 import {InputTextModule} from "primeng/inputtext";
@@ -13,10 +13,14 @@ import {PaginatorModule} from "primeng/paginator";
 import {StepperModule} from "primeng/stepper";
 import {NgForOf, NgIf} from "@angular/common";
 import {AuthService} from "../../shared/auth/auth.service";
-import {CreateJobResponse, JobService} from "../../shared/job/job.service";
-import {LanguageDto, LanguageService} from "../../shared/language/language.service";
+import {JobService} from "../../shared/job/job.service";
+import {LanguageService} from "../../shared/language/language.service";
 import {CheckboxModule} from "primeng/checkbox";
 import {JobsComponent} from "../jobs/jobs.component";
+import {PanelModule} from "primeng/panel";
+import {MenuModule} from "primeng/menu";
+import {CoursePanelComponent} from "../course-panel/course-panel.component";
+import {UrlSegmentService} from "../../shared/url/url-segment.service";
 
 @Component({
   selector: 'app-courses',
@@ -34,7 +38,10 @@ import {JobsComponent} from "../jobs/jobs.component";
     NgIf,
     CheckboxModule,
     NgForOf,
-    JobsComponent
+    JobsComponent,
+    PanelModule,
+    MenuModule,
+    CoursePanelComponent
   ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.sass',
@@ -47,152 +54,25 @@ import {JobsComponent} from "../jobs/jobs.component";
     JobService
   ]
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
   courses!: CourseDto[];
-  languages: LanguageDto[] = [];
-  waitForJobGuids: string[] = [];
-  selectedLanguageIds: number[] = [];
 
-  deleteDialogVisible = false;
-  cloneDialogVisible = false;
-  cloning = false;
-  refreshPdfDialogVisible = false;
-  generatingTexFiles = false;
-  jobsAreDone = false;
-
-  selectedCourse: CourseDto | undefined;
-  cloneCourseDto: CloneCourseDto = {
-    name: undefined,
-    abbreviation: undefined,
-    pruefungsordnung: undefined
-  }
+  items: MenuItem[] | undefined;
 
   constructor(private departmentService: DepartmentService,
-              private courseService: CourseService,
-              private jobService: JobService,
-              private languageService: LanguageService,
-              private router: Router,
-              private route: ActivatedRoute,
-              protected authService: AuthService,
-              private messageService: MessageService) {
+              private segmentService: UrlSegmentService
+              ) {
   }
 
   ngOnInit() {
+    this.loadCourses();
+  }
 
-    const url = this.router.url;
-    const segments = url.split("/");
-
-    const getIdFromSegment = (segment: string) => {
-      const segmentIndex = segments.indexOf(segment);
-      return segmentIndex !== -1 ? segments[segmentIndex + 1] : undefined;
-    };
-
-
-    const departmentId = Number(getIdFromSegment("department"));
+  loadCourses() {
+    const departmentId = Number(this.segmentService.getIdFromSegment("department"));
 
     this.departmentService.get(departmentId).subscribe(department => {
       this.courses = department.degreePrograms;
     });
-
-    this.languageService.getLanguages().subscribe(languages => {
-      this.languages = languages;
-      this.selectedLanguageIds = languages.map(language => language.id);
-    });
-  }
-
-  async selectCourse(course: CourseDto) {
-    await this.router.navigate(['course', course.id], {relativeTo: this.route});
-  }
-
-  cloneCourse(course: CourseDto, $event: MouseEvent) {
-    $event.stopPropagation();
-
-    this.selectedCourse = course;
-    this.cloneCourseDto = {
-      name: course.translations[0].name,
-      abbreviation: course.abbreviation,
-      pruefungsordnung: course.translations[0].pruefungsordnung
-    }
-    console.log(course);
-
-    this.cloneDialogVisible = true;
-  }
-
-  deleteCourse(course: CourseDto, $event: MouseEvent) {
-    $event.stopPropagation();
-
-    this.selectedCourse = course;
-    this.deleteDialogVisible = true;
-  }
-
-  async onCourseDeleted() {
-    if (!this.selectedCourse) return;
-
-    this.courseService.delete(this.selectedCourse.id).subscribe(() => {
-      this.courses = this.courses.filter(course => course.id !== this.selectedCourse!.id);
-      this.deleteDialogVisible = false;
-    });
-  }
-
-  async onCourseCloned() {
-    if (!this.selectedCourse) return;
-    this.cloning = true;
-
-    this.courseService.cloneCourse(this.selectedCourse.id, this.cloneCourseDto).subscribe((newCourseId: number) => {
-      this.courseService.get(newCourseId).subscribe(
-        course => {
-          this.courses.push(course);
-          this.cloneDialogVisible = false;
-          this.cloning = false;
-        }
-      );
-    });
-  }
-
-  downloadPdf(course: CourseDto, $event: MouseEvent) {
-    $event.stopPropagation();
-
-    this.languageService.getLanguageByCode(this.languageService.languageCode).subscribe(language => {
-        window.open(this.courseService.downloadPdfUrl(course.id, language[0].id), '_blank');
-    });
-  }
-
-  refreshPdf(course: CourseDto, $event: MouseEvent) {
-    $event.stopPropagation();
-    this.selectedCourse = course;
-    this.refreshPdfDialogVisible = true;
-  }
-
-  startPdfGeneration(nextCallback: any) {
-    nextCallback.emit();
-
-    this.generatingTexFiles = true;
-    let finished = 0;
-
-    this.selectedLanguageIds.forEach(languageId => {
-      this.jobService.createNew(languageId, this.selectedCourse?.id!).subscribe((response: CreateJobResponse) => {
-        finished++;
-        const guid = response.guid;  // Extract the guid property
-        this.waitForJobGuids = [...this.waitForJobGuids, guid];
-        if (finished === this.selectedLanguageIds.length) {
-          this.generatingTexFiles = false;
-          this.messageService.add({severity: 'success', summary: 'Success', detail: 'PDF generation started'});
-        }
-      });
-    });
-  }
-
-  confirmPdfPublish() {
-    this.refreshPdfDialogVisible = false;
-    this.messageService.add({severity: 'success', summary: 'Success', detail: 'PDF successfully published'});
-  }
-
-  cancelPdfPublish() {
-    this.refreshPdfDialogVisible = false;
-  }
-
-  onJobsDone() {
-    this.jobsAreDone = true;
   }
 }
-
