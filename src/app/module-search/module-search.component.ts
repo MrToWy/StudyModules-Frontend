260393c79d4e09329d2ea7ee5c8e07742d619653
Tobias Wylega <github@wylega.de>
2024-08-12 13:58:16 +0200

    refactor

--- src/app/module-search/module-search.component.ts
+++ src/app/module-search/module-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {AutoCompleteModule} from "primeng/autocomplete";
 import {FilterService, SelectItemGroup} from "primeng/api";
@@ -11,8 +11,8 @@ import {CourseService} from "../../shared/course/course.service";
 import {activeTranslationIndex} from "../module-translator/module-translator.component";
 
 interface AutoCompleteCompleteEvent {
-    originalEvent: Event;
-    query: string;
+  originalEvent: Event;
+  query: string;
 }
 
 @Component({
@@ -29,79 +29,86 @@ interface AutoCompleteCompleteEvent {
   styleUrl: './module-search.component.sass'
 })
 export class ModuleSearchComponent {
-selectedModule: any;
+  selectedModule: any;
 
-    filteredModules!: any[];
+  filteredModules!: any[];
 
-    groupedModules!: SelectItemGroup[];
+  groupedModules!: SelectItemGroup[];
 
-    constructor(
-      private router: Router,
-      private filterService: FilterService,
-      private moduleService: ModuleService,
-      private courseService: CourseService
-      ) { }
+  constructor(
+    private router: Router,
+    private filterService: FilterService,
+    private moduleService: ModuleService,
+    private courseService: CourseService
+  ) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-      this.courseService.getAll().subscribe(courses => {
-        this.groupedModules = courses.map(course => {
+    this.courseService.getAll().subscribe(courses => {
+      this.groupedModules = courses.map(course => {
+        const courseLabel = course?.translations?.[activeTranslationIndex]?.name;
+        const coursePath = `/faculty/${course.department.facultyId}/department/${course.department.id}/course/${course.id}`;
+
+        const items = course.modules.map(module => {
           return {
-            label: course?.translations?.at(activeTranslationIndex)?.name ?? '',
-            items: course.modules.map(module => {
-              return {
-                label: module.translations?.at(activeTranslationIndex)?.name,
-                value: '/faculty/' + course.department.facultyId + '/department/' + course.department.id + '/course/' + course.id + '/module/' + module.id
-              }
-            })
-          }
+            label: module.translations?.[activeTranslationIndex]?.name,
+            value: `${coursePath}/module/${module.id}`
+          };
         });
+
+        return {
+          label: courseLabel,
+          items: items
+        };
       });
+    });
+
+  }
+
+  filterModule(event: AutoCompleteCompleteEvent) {
+
+    if (!this.groupedModules) {
+      return;
     }
 
-    filterModule(event: AutoCompleteCompleteEvent) {
+    let query = event.query;
+    let filteredGroups = [];
 
-      if(!this.groupedModules) {
+    for (let optgroup of this.groupedModules) {
+      let filteredSubOptions = this.filterService.filter(optgroup.items, ['label'], query, "contains");
+      if (filteredSubOptions && filteredSubOptions.length) {
+        filteredGroups.push({
+          label: optgroup.label,
+          value: optgroup.value,
+          items: filteredSubOptions
+        });
+      }
+    }
+
+    this.filteredModules = filteredGroups;
+  }
+
+  async onSelect(event: any) {
+    await this.router.navigate([event.value.value]);
+    window.location.reload();
+  }
+
+  async onKey(event: any) {
+    if (event.key === "Enter") {
+
+      if (
+        this.filteredModules === undefined ||
+        this.filteredModules?.length === 0 ||
+        this.filteredModules[0].items.length === 0 ||
+        this.filteredModules[0].items[0].value === undefined) {
+
+        await this.router.navigate(['/']);
         return;
       }
 
-        let query = event.query;
-        let filteredGroups = [];
-
-        for (let optgroup of this.groupedModules) {
-            let filteredSubOptions = this.filterService.filter(optgroup.items, ['label'], query, "contains");
-            if (filteredSubOptions && filteredSubOptions.length) {
-                filteredGroups.push({
-                    label: optgroup.label,
-                    value: optgroup.value,
-                    items: filteredSubOptions
-                });
-            }
-        }
-
-        this.filteredModules = filteredGroups;
-    }
-
-    async onSelect(event: any) {
-      await this.router.navigate([event.value.value]);
+      await this.router.navigate([this.filteredModules[0].items[0].value]);
       window.location.reload();
     }
-
-    async onKey(event: any) {
-      if(event.key === "Enter") {
-
-        if(
-          this.filteredModules === undefined ||
-          this.filteredModules?.length === 0 ||
-          this.filteredModules[0].items.length === 0 ||
-          this.filteredModules[0].items[0].value === undefined) {
-
-          await this.router.navigate(['/']);
-          return;
-        }
-
-        await this.router.navigate([this.filteredModules[0].items[0].value]);
-        window.location.reload();
-      }
-    }
+  }
 }
